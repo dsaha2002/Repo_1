5949f6c7afc35c7231ca6b48383ece40074fa890
Debajyoti <dsaha@gbtechservice.com>
2025-07-18 15:39:13 +0530

    Cypress Code

---
+++ cypress/e2e/Test_1.cy.js
@@ -0,0 +1,13 @@
+const cypress = require("cypress");
+const { describe } = require("mocha");
+
+describe('My First Test', () =>
+{
+    it('test 1', () => {
+
+        cy.visit("https://www.flipkart.com/")
+        cy.title().should('eq', "Flipkart")
+
+    }
+    )
+})
